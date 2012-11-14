d9c30479e52bef1e4b34e00b5c24f5ca1e1eb703
Wes Williams <wwwestech@gmail.com>
2012-11-13 18:21:56 -0800

    cleanup and comments

--- grails-app/services/graphviz/SlcService.groovy
+++ grails-app/services/graphviz/SlcService.groovy
@@ -9,9 +9,11 @@ import graphviz.oauth.OAuth20ServiceBearerAdapter
 
 class SlcService {
 
+    // start constants
     final String API_BASE_URL = 'https://api.sandbox.slcedu.org/api/rest/v1'
     final String SCHOOL_KEY_PREFIX = 'Schools_'
-
+    // end constants
+    
     OauthService oauthService
     boolean bearerEnabled=false // part of authorization hack
     
@@ -21,6 +23,7 @@ class SlcService {
       session[key]
     }
         
+    // all rest calls must go through here
     def call(verb,url) {
     
       Token accessToken = retrieveFromSession(oauthService.findSessionKeyForAccessToken('slc'))    
@@ -43,62 +46,7 @@ class SlcService {
       JSON.parse(response.body)
     } 
     
-    def extractContent(data) {
-      
-      def content = extractContent(data,null).toString()
-      content = content.replaceAll("<br /><br /><br />","<br />")
-      content = content.replaceAll("<br /><br />","<br />")
-      content = content.replaceAll("<hr /><hr />","<hr />")
-      
-      return "<div style='width:420px;height:300px;overflow:scroll;white-space:nowrap;'>${content}</div>"
-    }
-    
-    def extractContent(data,contentIn) {
-        
-      StringBuilder content = contentIn!=null?contentIn:new StringBuilder()
-     
-       if(data instanceof List) {
-
-         if(data.size() > 0 ) {
-           data.each { 
-             extractContent(it,content)  
-           }
-         }
-       }
-       else if(data instanceof Map) {
-         data.each { k , v ->
-           if(k != "links") {
-             if(!(v instanceof List<Map> && v.size() == 0)) {      
-               if(v instanceof Map || v instanceof List) {
-                 content.append("<hr />")
-                 content.append("<b>${k.capitalize().replaceAll(/([A-Z][a-z]*)/, '$1 ').trim().toUpperCase()}:</b> ")
-                 content.append("<hr /><i>")
-               }
-               else {
-                 if(k==k.toUpperCase()) {
-                   content.append("<b>${k}:</b> ")
-                 }
-                 else {
-                   content.append("<b>${k.capitalize().replaceAll(/([A-Z][a-z]*)/, '$1 ').trim()}:</b> ")
-                 }
-               }
-               extractContent(v,content)
-               content.append("<br />")
-               if(v instanceof Map || v instanceof List) {
-                 content.append("</i><hr />")
-               }
-             } 
-           } 
-         }
-       }
-       else if(data!=null) {
-         content.append(data)
-         content.append("<br />")
-       }
-       
-       return content
-    }
-    
+    // entry point for controller
     def lookupData(searchId) {
     
       println "API lookup for ${searchId}"
@@ -108,14 +56,20 @@ class SlcService {
       }
            
        def idParts = searchId.tokenize('_')
-       def entityName = idParts[0]
-       def entityArgs = idParts.drop(1)
+       def entityName = idParts[0]        // first token is entiry
+       def entityArgs = idParts.drop(1)   // everything is argument
        
+       // find method should exist for each Entity
+       // If not, methodMissing provides dummy impl for now
        "find${entityName}"(*entityArgs)
     }
     
+    // the center of everything
     def findSLC() {
     
+      // TODO - need to make this method user centric
+      // base impl on user's role
+      
       def slcView = [:]  
       slcView['id'] = 'slc'
       slcView['label'] = 'SLC'
@@ -128,10 +82,13 @@ class SlcService {
 	  return slcView
     }
     
+    // find a certain school
     def findSchools(schoolId) {
       getAllSchools(schoolId)[0]
     }
    
+    // find all or certain school
+    // NOTE: schoolId is optional
     def getAllSchools(schoolId) {
     
       def schoolUrl = "${API_BASE_URL}/schools/"
@@ -143,6 +100,7 @@ class SlcService {
       def schoolList = []      
       def schoolData = call('get',schoolUrl)
       
+      // wrap single school in List
       if(schoolId !=null) {
          schoolData = [schoolData]
       }
@@ -166,7 +124,7 @@ class SlcService {
           schoolView['id'] = SCHOOL_KEY_PREFIX + schoolId
         }
         else {
-          schoolView['id'] = SCHOOL_KEY_PREFIX + (selfLink[0].href - schoolUrl.toString())
+          schoolView['id'] = SCHOOL_KEY_PREFIX + school.id
         }
         
         schoolView['label'] = school.educationOrgIdentificationCode[0].ID
@@ -174,7 +132,7 @@ class SlcService {
 	    schoolView['summary'] = "SLC Identity # ${school.id}"
 	    schoolView['content'] = extractContent(school)
 	    schoolView['imageUrl'] = null
-	    schoolView['relations'] = getLinks.collect { (it.rel - 'get') + '_' + (schoolView.id - SCHOOL_KEY_PREFIX)  }
+	    schoolView['relations'] = getLinks.collect { (it.rel - 'get') + '_' + school.id  }
 
 	    schoolList.add(schoolView)
       }
@@ -182,18 +140,18 @@ class SlcService {
       return schoolList
     }
     
-    
-     def methodMissing(String name, args) {
+    // provide a temp impl for all non-implemented entities
+    def methodMissing(String name, args) {
       
        if(name =~ /^find.+/) {
          def missingView = [:]  
          def entityName = name - 'find'
-         def entityNameWithSpaces = entityName.replaceAll(/([A-Z][a-z]*)/, '$1 ').trim()
+         def entityNameWithSpaces = spaceCamelCase(entityName)
 	     missingView['id'] = entityName + '_' + args.join('_')
 		 missingView['label'] = entityNameWithSpaces
 		 missingView['title'] = entityNameWithSpaces
-	     missingView['summary'] = ''
-	     missingView['content'] = ''
+	     missingView['summary'] = 'TODO'
+	     missingView['content'] = 'Not implemented yet'
 	     missingView['imageUrl'] = null
 	     missingView['relations'] = []
 	     
@@ -203,4 +161,70 @@ class SlcService {
        }
        else throw new MissingMethodException(name, delegate, args)
    }
+   
+   // start collection of string utils
+  
+   def eliminateRepeats(str,repeatedString) {
+     str.replaceAll(~"(${repeatedString}){2,}",repeatedString)
+   }
+   
+   def spaceCamelCase(str) {
+     str.capitalize().replaceAll(/([A-Z][a-z]*)/, '$1 ').trim()
+   }
+    
+   def extractContent(data) {
+      
+      def content = extractContent(data,null).toString()
+      content = eliminateRepeats(content,"<br />")
+      content = eliminateRepeats(content,"<hr />")
+      
+      return "<div style='width:420px;height:300px;overflow:scroll;white-space:nowrap;'>${content}</div>"
+    }
+    
+    def extractContent(data,contentIn) {
+        
+      StringBuilder content = contentIn!=null?contentIn:new StringBuilder()
+     
+       if(data instanceof List) {
+
+         if(data.size() > 0 ) {
+           data.each { 
+             extractContent(it,content)  
+           }
+         }
+       }
+       else if(data instanceof Map) {
+         data.each { k , v ->
+           if(k != "links") {
+             if(!(v instanceof List<Map> && v.size() == 0)) {      
+               if(v instanceof Map || v instanceof List) {
+                 content.append("<hr />")
+                 content.append("<b>${spaceCamelCase(k).toUpperCase()}:</b> ")
+                 content.append("<hr /><i>")
+               }
+               else {
+                 if(k==k.toUpperCase()) {
+                   content.append("<b>${k}:</b> ")
+                 }
+                 else {
+                   content.append("<b>${spaceCamelCase(k)}:</b> ")
+                 }
+               }
+               extractContent(v,content)
+               content.append("<br />")
+               if(v instanceof Map || v instanceof List) {
+                 content.append("</i><hr />")
+               }
+             } 
+           } 
+         }
+       }
+       else if(data!=null) {
+         content.append(data)
+         content.append("<br />")
+       }
+       
+       return content
+    }
+    // end collection of string utils
 } 
